0a1ebc76760a2360974a1ece0f87b3090a443950
svetlanalf <svetlanalf.by@gmail.com>
2021-08-22 22:07:17 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) { 
+  let str = Math.abs(n).toString();
+  let resultNum = 0;
+  for (let i = 0; i < str.length; i += 1){
+    resultNum += str[i] * Math.pow(10,i);
+  }
+
+  return resultNum;
+
 }
